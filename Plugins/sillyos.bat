98b71ecda91a7fd7336d3151ffaeb67e2d0bf96e
Coolbeansguy <157984205+Coolbeansguy@users.noreply.github.com>
2026-01-26 13:12:48 -0600

    Update SillyOS to version 1.8 with improvements

--- Plugins/sillyos.bat
+++ Plugins/sillyos.bat
@@ -3,10 +3,9 @@ title SillyOS Desktop
 color 17
 cls
 
-:: --- SAFETY PAUSE ---
-:: If the script crashes instantly, this pause will catch it so you can see why.
-:: Once it works, you can remove this line.
-echo [DEBUG] SillyOS is starting...
+:: --- DEBUG PAUSE ---
+:: If it crashes, this lets you see why.
+echo [DEBUG] Loading SillyOS...
 timeout /t 1 >nul
 
 :: --- MAIN MENU ---
@@ -14,7 +13,7 @@ timeout /t 1 >nul
 cls
 echo.
 echo   _________________________________________________________
-echo  |  SILLY OS v1.7                        User: Admin     |
+echo  |  SILLY OS v1.8                        User: Admin     |
 echo  |_________________________________________________________|
 echo.
 echo      .---.          .---.          .---.          .---.
@@ -24,12 +23,12 @@ echo    [1] Apps       [2] Store      [3] Info       [4] Log Out
 echo.
 echo   _________________________________________________________
 echo.
-set /p "os_choice=Command > "
+set /p os_choice=Command^> 
 
-if "%os_choice%"=="1" goto apps
-if "%os_choice%"=="2" goto launch_store
-if "%os_choice%"=="3" goto info
-if "%os_choice%"=="4" goto logout
+if %os_choice%==1 goto apps
+if %os_choice%==2 goto launch_store
+if %os_choice%==3 goto info
+if %os_choice%==4 goto logout
 goto desktop
 
 :apps
@@ -40,47 +39,33 @@ echo  1. AutoClicker
 echo  2. Matrix
 echo  3. Back
 echo.
-set /p "app=Select > "
-if "%app%"=="3" goto desktop
+set /p app=Select^> 
 
-:: LAUNCH LOGIC
-:: We assume we are in the main folder.
-if "%app%"=="1" (
-    if exist "Files\auto.EXE" (
-        start "" "Files\auto.EXE"
-    ) else (
-        echo [!] Missing Files\auto.EXE
-        pause
-    )
-    goto apps
-)
+if %app%==3 goto desktop
 
-if "%app%"=="2" (
-    if exist "Files\Plugins\matrix.bat" (
-        call "Files\Plugins\matrix.bat"
-    ) else (
-        echo [!] Missing Files\Plugins\matrix.bat
-        pause
-    )
-    goto apps
-)
+:: LAUNCH LOGIC (No Parentheses!)
+if %app%==1 goto run_auto
+if %app%==2 goto run_matrix
+goto apps
+
+:run_auto
+start Files\auto.EXE
+goto apps
+
+:run_matrix
+call Files\Plugins\matrix.bat
 goto apps
 
 :launch_store
-if exist "Files\Plugins\store.bat" (
-    call "Files\Plugins\store.bat"
-) else (
-    echo [!] Store plugin not found.
-    pause
-)
+call Files\Plugins\store.bat
 goto desktop
 
 :info
 cls
 echo.
 echo  [ SYSTEM INFO ]
-echo  OS: SillyOS v1.7
-echo  Mode: Safe Start
+echo  OS: SillyOS v1.8
+echo  Status: Working
 echo.
 pause
 goto desktop
