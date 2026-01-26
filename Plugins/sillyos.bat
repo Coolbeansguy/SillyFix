5f130a27a481d286e811ea1095030e18f0194ce3
Coolbeansguy <157984205+Coolbeansguy@users.noreply.github.com>
2026-01-26 13:15:11 -0600

    Fix input handling and update to SillyOS v1.9
    Updated version to v1.9 and improved input handling.

--- Plugins/sillyos.bat
+++ Plugins/sillyos.bat
@@ -3,8 +3,6 @@ title SillyOS Desktop
 color 17
 cls
 
-:: --- DEBUG PAUSE ---
-:: If it crashes, this lets you see why.
 echo [DEBUG] Loading SillyOS...
 timeout /t 1 >nul
 
@@ -12,23 +10,27 @@ timeout /t 1 >nul
 :desktop
 cls
 echo.
-echo  _________________________________________________________
-echo  |  SILLY OS v1.8                        User: Admin       |
-echo  |_________________________________________________________|
+echo   _________________________________________________________
+:: FIX: We added ^ before the pipes (|)
+echo  ^|  SILLY OS v1.9                        User: Admin     ^|
+echo  ^|_________________________________________________________^|
 echo.
 echo      .---.          .---.          .---.          .---.
-echo     | [_] |        |  $  |        |  ?  |        |  X  |
+echo     ^| [_] ^|        ^|  $  ^|        ^|  ?  ^|        ^|  X  ^|
 echo     '-----'        '-----'        '-----'        '-----'
 echo    [1] Apps       [2] Store      [3] Info       [4] Log Out
 echo.
 echo   _________________________________________________________
 echo.
-set /p os_choice=Command^> 
 
-if %os_choice%==1 goto apps
-if %os_choice%==2 goto launch_store
-if %os_choice%==3 goto info
-if %os_choice%==4 goto logout
+:: FIX: Safer input handling
+set "os_choice="
+set /p "os_choice=Command > "
+
+if "%os_choice%"=="1" goto apps
+if "%os_choice%"=="2" goto launch_store
+if "%os_choice%"=="3" goto info
+if "%os_choice%"=="4" goto logout
 goto desktop
 
 :apps
@@ -39,33 +41,32 @@ echo  1. AutoClicker
 echo  2. Matrix
 echo  3. Back
 echo.
-set /p app=Select^> 
+set "app="
+set /p "app=Select > "
 
-if %app%==3 goto desktop
-
-:: LAUNCH LOGIC (No Parentheses!)
-if %app%==1 goto run_auto
-if %app%==2 goto run_matrix
+if "%app%"=="3" goto desktop
+if "%app%"=="1" goto run_auto
+if "%app%"=="2" goto run_matrix
 goto apps
 
 :run_auto
-start Files\auto.EXE
+start "" "Files\auto.EXE"
 goto apps
 
 :run_matrix
-call Files\Plugins\matrix.bat
+call "Files\Plugins\matrix.bat"
 goto apps
 
 :launch_store
-call Files\Plugins\store.bat
+call "Files\Plugins\store.bat"
 goto desktop
 
 :info
 cls
 echo.
 echo  [ SYSTEM INFO ]
-echo  OS: SillyOS v1.8
-echo  Status: Working
+echo  OS: SillyOS v1.9 (Fixed Art)
+echo  Status: Operational
 echo.
 pause
 goto desktop
