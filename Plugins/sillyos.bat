b3b416e250f3125f524d98fc51dd90103e49da3e
Coolbeansguy <157984205+Coolbeansguy@users.noreply.github.com>
2026-01-26 13:05:16 -0600

    Update SillyOS batch script to version 1.7

--- Plugins/sillyos.bat
+++ Plugins/sillyos.bat
@@ -1,42 +1,20 @@
 @echo off
-setlocal enabledelayedexpansion
-color 17
 title SillyOS Desktop
+color 17
+cls
 
-:: --- STEP 1: FIND THE ROOT FOLDER ---
-:: We still need this so the Apps (AutoClicker, Store) work correctly.
+:: --- SAFETY PAUSE ---
+:: If the script crashes instantly, this pause will catch it so you can see why.
+:: Once it works, you can remove this line.
+echo [DEBUG] SillyOS is starting...
+timeout /t 1 >nul
 
-if exist "SillyFix.bat" goto :found_root
-if exist "..\..\SillyFix.bat" goto :move_up_2
-if exist "..\SillyFix.bat" goto :move_up_1
-
-:: Fallback: If we can't find it, just proceed and hope for the best.
-goto :found_root
-
-:move_up_2
-cd ..\..
-goto :found_root
-
-:move_up_1
-cd ..
-goto :found_root
-
-:found_root
-:: --- STEP 2: SKIP LOGIN ---
-:: No saving, no reading. Just set the name in memory.
-set "username=Admin"
-
-:: Go straight to the desktop!
-goto :desktop
-
+:: --- MAIN MENU ---
 :desktop
 cls
-color 17
-title SillyOS Desktop - User: %username%
-
 echo.
 echo   _________________________________________________________
-echo  |  SILLY OS v1.6                        User: %username%     |
+echo  |  SILLY OS v1.7                        User: Admin     |
 echo  |_________________________________________________________|
 echo.
 echo      .---.          .---.          .---.          .---.
@@ -48,16 +26,11 @@ echo   _________________________________________________________
 echo.
 set /p "os_choice=Command > "
 
-if "%os_choice%"=="1" goto :apps
-if "%os_choice%"=="2" goto :launch_store
-if "%os_choice%"=="3" goto :info
-if "%os_choice%"=="4" goto :logout
-goto :desktop
-
-:launch_store
-:: We use call so it returns here when done
-call "Files\Plugins\store.bat"
-goto :desktop
+if "%os_choice%"=="1" goto apps
+if "%os_choice%"=="2" goto launch_store
+if "%os_choice%"=="3" goto info
+if "%os_choice%"=="4" goto logout
+goto desktop
 
 :apps
 cls
@@ -68,23 +41,53 @@ echo  2. Matrix
 echo  3. Back
 echo.
 set /p "app=Select > "
-if "%app%"=="3" goto :desktop
-if "%app%"=="1" start "" "Files\auto.EXE" & goto :apps
-if "%app%"=="2" call "Files\Plugins\matrix.bat" & goto :apps
-goto :apps
+if "%app%"=="3" goto desktop
+
+:: LAUNCH LOGIC
+:: We assume we are in the main folder.
+if "%app%"=="1" (
+    if exist "Files\auto.EXE" (
+        start "" "Files\auto.EXE"
+    ) else (
+        echo [!] Missing Files\auto.EXE
+        pause
+    )
+    goto apps
+)
+
+if "%app%"=="2" (
+    if exist "Files\Plugins\matrix.bat" (
+        call "Files\Plugins\matrix.bat"
+    ) else (
+        echo [!] Missing Files\Plugins\matrix.bat
+        pause
+    )
+    goto apps
+)
+goto apps
+
+:launch_store
+if exist "Files\Plugins\store.bat" (
+    call "Files\Plugins\store.bat"
+) else (
+    echo [!] Store plugin not found.
+    pause
+)
+goto desktop
 
 :info
 cls
 echo.
 echo  [ SYSTEM INFO ]
-echo  OS: SillyOS v1.6 (No Login)
-echo  Location: %CD%
+echo  OS: SillyOS v1.7
+echo  Mode: Safe Start
 echo.
 pause
-goto :desktop
+goto desktop
 
 :logout
 cls
 echo.
 echo  Logging out...
+timeout /t 1 >nul
 exit /b
